1a7bfe832c281665555c88bc46fedaff1da98f47
woongminKi <dndals1991@gmail.com>
2024-03-11 18:25:45 +0900

    fix: wss 하드로 설정

--- src/components/exchanges/Main.js
+++ src/components/exchanges/Main.js
@@ -355,7 +355,9 @@ export default function Main() {
   }, [coinData]);
 
   useEffect(() => {
-    const ws = new WebSocket(process.env.REACT_APP_WEBSOCKET_SERVER_URL);
+    const ws = new WebSocket(
+      'wss://' + process.env.REACT_APP_WEBSOCKET_SERVER_URL
+    );
 
     ws.onmessage = (event) => {
       const res = JSON.parse(event.data);
