3476ee430281093c08fe18261eb65dde4fed7f3b
woongminKi <dndals1991@gmail.com>
2024-03-25 22:02:32 +0900

    wss test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './app/App';
 import store from './app/configureStore';
 import { BrowserRouter } from 'react-router-dom';
