16010a7c5475b4d7d68e19c91c6dd103f67ab639
woongminKi <dndals1991@gmail.com>
2024-04-18 23:33:47 +0900

    fix: kakao redirect uri 배포 버전으로 교체

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,10 +4,11 @@ import kakaoImage from '../images/kakao_login_medium_narrow.png';
 
 export default function Login() {
   const clientId = process.env.REACT_APP_API_ID;
-  // const redirectURI = `${process.env.REACT_APP_REDIRECT_URI}/oauth/kakao/callback`;
-  const redirectURI = `http://localhost:3000/oauth/kakao/callback`;
+  const redirectURI = `${process.env.REACT_APP_REDIRECT_URI}/oauth/kakao/callback`;
+  // const redirectURI = `http://localhost:3000/oauth/kakao/callback`;
   // const redirectURI = `https://coin-is-easy.xyz/oauth/kakao/callback`;
-  console.log('redirectURI::', redirectURI);
+  // console.log('redirectURI::', redirectURI);
+
   // const clientId = '736c88fab71bf57de5334a344133d9f9';
   // const redirectURI = `http://localhost:3000/oauth/kakao/callback`;
   // const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectURI}&response_type=code`;
